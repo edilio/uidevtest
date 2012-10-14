7a79ffa3a4cb73e760318938defd8a56a8d3cbc7
Edilio Gallardo <edilio@gmail.com>
2012-10-14 19:18:41 -0400

    improvement in css for list view

--- src/js/uitest.js
+++ src/js/uitest.js
@@ -13,19 +13,70 @@ var app = angular.module('uitest', []).
     });
 
 
-function ctrl($scope,  $http) {
+app.factory('ui_data', ['$http',function($http) {
+    var stories = [];
 
-    $scope.stories = [];
+    var insertIndex = function (storyList){
+        var injectIndex = function (item) {
+            item["index"] = 'sto0' + (storyList.indexOf(item)+1).toString(10);
+            return item;
+        };
 
-    $http.get('../js/uidevtest-data.js').success(function(data){
-        $scope.stories = data.objects;
-    });
+        return storyList.map(injectIndex);
+    };
+
+    return {
+        currentStory : {},
+        stories : stories,
+        getStoryByIndex : function(index){
+            var i = parseInt(index[index.length-1]) - 1;
+            return stories[i];
+        },
+        insertIndex : insertIndex,
+        setStories : function (scopeStories){
+            stories = scopeStories;
+        }
+    };
+}]);
+
+
+
+function ctrl($scope,  $http, ui_data) {
+
+    $scope.currentStory = ui_data.currentStory;
+
+    $scope.stories = ui_data.stories;
 
     $scope.formatDate = function(value){
         var d = new Date(value);
         return d.toString('hh:mm tt dddd, MMM. d, yyyy');
     };
 
+    var search = window.location.search;
 
+    if ((search === undefined) ||(search === "")){
+        $scope.showList = true;
+        $scope.currentStory = undefined;
+    }
+    else{
+        $scope.showList = false;
+        $scope.storyIndex = parseInt(search[search.length-1]) - 1;
+        $scope.currentStory = $scope.stories[$scope.storyIndex];
+    }
+
+    $scope.showDetail = function (story){
+        $scope.showList = false;
+        $scope.currentStory = story;
+        data.currentStory = story;
+    };
+
+    if ($scope.stories.length === 0)
+        $http.get('../js/uidevtest-data.js').success(function(data){
+            $scope.stories = ui_data.insertIndex(data.objects);
+            ui_data.setStories($scope.stories);
+
+            $scope.currentStory = ui_data.getStoryByIndex(search);
+            $('#currentStory').html($scope.currentStory.story);
+        });
 }
 
